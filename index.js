f7b7c7ab001fa8731f699c86499383b7ff37e98a
jswildcards <eggkuv35c@relay.firefox.com>
2021-09-28 09:49:38 +0800

    feat: schedule link with calendar

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 new Vue({
   el: "#app",
-  data: {
-    attrs: [
-      {
-        key: "today",
-        highlight: true,
-        dates: new Date(),
-      },
-    ],
+  data: function () {
+    return {
+      attrs: [
+        {
+          key: "today",
+          highlight: true,
+          dates: new Date(),
+        },
+      ],
+      schedule: {},
+      show: [],
+    };
+  },
+  mounted: async function () {
+    const schedule = await fetch(
+      "https://raw.githubusercontent.com/jswildcards/schedule/main/schedule.json"
+    ).then((res) => res.json());
+
+    this.schedule = schedule.reduce((prev, cur) => {
+      cur.dates.forEach((date) => {
+        prev = {
+          ...prev,
+          [date]: [...(prev[date] ?? []), cur.id],
+        };
+      });
+
+      return prev;
+    }, {});
+
+    console.log(Object.keys(this.schedule).length);
+  },
+  methods: {
+    selectDate: function (e) {
+      this.show = schedule[e.id] ?? [];
+    },
   },
 });
